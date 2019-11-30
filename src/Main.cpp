46fd9e100acbe5aaf675bf05168f7b578678abdc
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2019-11-30 12:25:30 +0000

    Added support for basic Diffuse materials

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,22 +2,32 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include "random.h"
 
 #include <iostream>
 #include <cstdlib>
 
-inline double random_double()
+vec3 random_in_unit_sphere()
 {
-	return rand() / (RAND_MAX + 1.0);
+	vec3 p;
+
+	do 
+	{
+		p = 2.0 * vec3(random_double(), random_double(), random_double()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+
+	return p;
 }
 
 vec3 color(const ray &r, hittable *world)
 {
 	hit_record rec;
 
-	if (world->hit(r, 0.0, FLT_MAX, rec))
+	if (world->hit(r, 0.001, FLT_MAX, rec))
 	{
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+
+		return 0.5 * color(ray(rec.p, target - rec.p), world);
 	}
 	else
 	{
@@ -61,6 +71,7 @@ int main()
 			}
 
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
