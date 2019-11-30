71eec4542bee09acd80933e2581105975a88f2f0
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2019-11-30 11:08:35 +0000

    Updated Raytracer
    * Refactored objects into thier own classes
    * Updated calculations for ray intersections
    * Added support for anti-aliasing

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,67 +1,66 @@
-#include "ray.h"
+#include "float.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
+#include <cstdlib>
 
-float hit_sphere(const vec3& center, float radius, const ray& r)
+inline double random_double()
 {
-	vec3 oc = r.origin() - center;
+	return rand() / (RAND_MAX + 1.0);
+}
 
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
+vec3 color(const ray &r, hittable *world)
+{
+	hit_record rec;
 
-	float discriminant = b * b - 4 * a * c;
-
-	if (discriminant < 0)
+	if (world->hit(r, 0.0, FLT_MAX, rec))
 	{
-		return -1.0;
+		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
 	}
 	else
 	{
-		return (-b - sqrt(discriminant)) / (2.0 * a);
+		vec3 unit_direction = unit_vector(r.direction());
+
+		float t = 0.5 * (unit_direction.y() + 1.0);
+
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
-}
-
-vec3 color(const ray &r)
-{
-	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-
-	if (t > 0.0)
-	{
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-
-		return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-	}
-
-	vec3 unit_direction = unit_vector(r.direction());
-
-	t = 0.5 * (unit_direction.y() + 1.0);
-
-	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main()
 {
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
 
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+	hittable *list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+
+	camera cam;
+
+	hittable *world = new hittable_list(list, 2);
 
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
+			vec3 col(0, 0, 0);
 
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			for (int s = 0; s < ns; s++)
+			{
+				float u = float(i + random_double()) / float(nx);
+				float v = float(j + random_double()) / float(ny);
 
-			vec3 col = color(r);
+				ray r = cam.get_ray(u, v);
+				col += color(r, world);
+			}
+
+			col /= float(ns);
 
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
